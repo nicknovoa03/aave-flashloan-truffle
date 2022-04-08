b2a9d4f0907b14d5a7cde14c01a646c6e6211149
nicknovoa03 <nicknovoa03@gmail.com>
2022-04-07 23:16:04 -0500

    first commit

--- src/truffle-config.js
+++ src/truffle-config.js
@@ -1,6 +1,6 @@
 const path = require("path");
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic2 = ""
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+var mnemonic = ""
 const mainnetUrl = "https://mainnet.infura.io/v3/bad8cc770bef49dc88683bf2290205c8";
 
 module.exports = {
@@ -9,7 +9,7 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
     live: {
-      provider: () => new HDWalletProvider(mnemonic2, mainnetUrl),
+      provider: () => new HDWalletProvider(mnemonic, mainnetUrl),
       network_id: 1
     },
     develop: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
@@ -19,7 +19,7 @@ module.exports = {
     },
     rinkeby: {
       provider: function () {
-        return new HDWalletProvider(mnemonic2, "https://rinkeby.infura.io/v3/bad8cc770bef49dc88683bf2290205c8");
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/bad8cc770bef49dc88683bf2290205c8");
       },
       network_id: 4,
       gas: 15000000,
@@ -29,7 +29,7 @@ module.exports = {
 
   compilers: {
     solc: {
-      version: "^0.8.0"
+      version: "0.8.10"
     }
   },
   api_keys: {
